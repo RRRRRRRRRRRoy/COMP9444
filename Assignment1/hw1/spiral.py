a7543300268a8301fcdd0266b41359a75ef9c7b7
RRRRRRRRRRRoy <RRRRRRRRRRRoy720@gmail.com>
2020-10-24 16:45:57 +0800

    Q5Answer

--- Assignment1/hw1/spiral.py
+++ Assignment1/hw1/spiral.py
@@ -114,8 +114,16 @@ class RawNet(torch.nn.Module):
         # CHANGE CODE HERE
         return final_output
 
+def check_number_layer(net, layer):
+    if(layer == 1):
+        mid_layer = net.h1
+    elif(layer == 2):
+        mid_layer = net.h2
+    return mid_layer
+
 def graph_hidden(net, layer, node):
     # INSERT CODE HERE
+    # These code is modified from graph_output.py
     xrange = torch.arange(start=-7,end=7.1,step=0.01,dtype=torch.float32)
     yrange = torch.arange(start=-6.6,end=6.7,step=0.01,dtype=torch.float32)
     xcoord = xrange.repeat(yrange.size()[0])
@@ -126,10 +134,7 @@ def graph_hidden(net, layer, node):
         net.eval()        # toggle batch norm, dropout
         output = net(grid)
         net.train() # toggle batch norm, dropout back again
-        if(layer == 1):
-            mid_layer = net.h1
-        elif(layer == 2):
-            mid_layer = net.h2
+        mid_layer = check_number_layer(net, layer)
         mid_layer_2_filtered = mid_layer[:,node]
         pred = (mid_layer_2_filtered >= 0).float()
 
